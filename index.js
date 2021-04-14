62da5218d26598c9edf935f03ba09a7558cbf7ee
dragon <tylerjseverino@gmail.com>
2021-04-14 19:33:37 -0400

    Part of merging leveling.js and index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Client }= require('discord.js-commando')
-const path = require('path')
-const fetch = require('node-fetch')
-const config = require ('./config')
+const { Client }= require('discord.js-commando');
+const path = require('path');
+const fetch = require('node-fetch');
+const config = require ('./config');
+const levelfunc = require('./utils/levelfunc');
 
 const client = new Client({
     commandPrefix: '$',
@@ -36,6 +37,21 @@ client.once('ready', () => {
 module.exports = client
 
 
+//Give a random ammount of XP every message.
+client.on("message", async (message) => {
+    if (!message.guild) return;
+    if (message.author.bot) return;
+      
+    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+  
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.author.id, message.guild.id);
+      message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+      levelfunc.checkLevel(user.level, message, message.author.id);
+      levelfunc.levelRole(user.level, message, message.author.id)
+    }
+  });
 
 
 
