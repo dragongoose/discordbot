0bb5ca6a491c5e7e4c91839931e9295294c3be3a
Dragon <tylerjseverino@gmail.com>
2021-04-15 23:24:35 -0400

    Speak 10 random words.

--- commands/jack/ttswords.js
+++ commands/jack/ttswords.js
@@ -2,6 +2,8 @@ const { Command } = require('discord.js-commando');
 const async = require('async');
 const fetch = require('node-fetch');
 const gTTS = require('gtts');
+const fs = require("fs")
+
 
 
 module.exports = class SayCommand extends Command {
@@ -22,12 +24,35 @@ module.exports = class SayCommand extends Command {
 
         var gtts = new gTTS(data.join(" "), 'en');
 
-        gtts.save('./commands/jack/ttsutil/tts.mp3', function (err, result) {
+        const randomnum = Math.floor(Math.random() * 100);
+
+        gtts.save('./commands/jack/ttsutil/' + randomnum + 'tts.mp3', function (err, result) {
             if (err) { throw new Error(err); }
             console.log("Text to speech converted!");
         });
 
+        if (msg.member.voice.channel) {
+            const connection = await msg.member.voice.channel.join();
+            const dispatcher = connection.play(`./commands/jack/ttsutil/${randomnum}tts.mp3`);
 
+            dispatcher.on('finish', () => {
+                console.log('Finished playing!');
+                msg.member.voice.channel.leave()
+
+            });
+
+        } else {
+            msg.channel.send('You need to be in a voice channel')
+        }
+
+
+        fs.unlink(`./commands/jack/ttsutil/${randomnum}tts.mp3`, function(err) {
+            if (err) {
+              throw err
+            } else {
+              console.log("Successfully deleted the file.")
+            }
+          })
 
     }
 };
