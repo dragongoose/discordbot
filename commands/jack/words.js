a027794edb4e8e974c909f65d44fdf82963604ba
Dragon <tylerjseverino@gmail.com>
2021-04-15 22:04:15 -0400

    Make randomwords better

---
+++ commands/jack/words.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+const async = require('async');
+const fetch = require('node-fetch');
+
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'words',
+			aliases: ['random words'],
+			group: 'jack',
+			memberName: 'words',
+			description: 'Says 10 random words.',
+			guildOnly: true,
+		})
+	}
+	async run(msg) {
+        
+        let asd = await fetch('https://random-word-api.herokuapp.com/word?number=10').then(response => response.text());
+        const data = eval(asd)
+
+        msg.channel.send(data.join(" "))
+
+
+	}
+};
