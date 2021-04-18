ce7a9f179b843c04aaf655601ac4e3dda25b1540
Dragon <tylerjseverino@gmail.com>
2021-04-18 00:40:13 -0400

    Add comments

--- commands/sound/time.js
+++ commands/sound/time.js
@@ -16,6 +16,7 @@ module.exports = class SayCommand extends Command {
     }
     run(msg) {
         try {
+            //define variables
             const time = dis.dispatcher.streamTime;
             const timetosecond = time / 1000
             const timeleft = dis.song.length - timetosecond
@@ -23,13 +24,15 @@ module.exports = class SayCommand extends Command {
             var timeone = Math.round(timeleft)// time elapsed
             var timetwo = dis.song.length// total time 
 
+            //get percentage for progressbar.js
             let endTime = timetwo
             let startTime = 0
             let now = timeone
             let totalTime = endTime - startTime;
             let progress = now - startTime;
             let percentage = 100 - ((progress / totalTime) * 100);
-
+            
+            //set embed for the time
             const embed = new Discord.MessageEmbed()
                 .setTitle('Current time')
                 .setDescription(`**${new Date(timeone * 1000).toISOString().substr(11, 8)} ${progressbar.progress(Math.round(percentage), '◽')} ${new Date(timetwo * 1000).toISOString().substr(11, 8)}**`)
@@ -38,14 +41,16 @@ module.exports = class SayCommand extends Command {
                 .setTimestamp();
 
             if (JSON.stringify(dis.connection.speaking) == 0) {
+                //change the emoji if the bot is paused
                 embed.setDescription(`**${new Date(timeone * 1000).toISOString().substr(11, 8)} ${progressbar.progress(Math.round(percentage), '⏸️')} ${new Date(timetwo * 1000).toISOString().substr(11, 8)}**`)
             }
-
+            //send the embed
             msg.channel.send(embed)
 
 
         } catch (e) {
             console.log(e)
+            //error catching
             if (e == "TypeError: Cannot read property 'streamTime' of undefined") {
                 msg.channel.send('Nothing is playing!')
             }
