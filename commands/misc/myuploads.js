619f204cd535ee900d67c154a66a8988924c62d4
Dragonshadow14 <tylerjseverino@gmail.com>
2021-07-25 21:48:14 -0400

    Start testing.

---
+++ commands/misc/myuploads.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord.js-commando')
+const Discord = require('discord.js')
+const { Database } = require("quickmongo");
+const db = new Database(config.dburl);
+
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'myuploads',
+			group: 'misc',
+			memberName: 'myuploads',
+			description: 'Lists your total uploads.',
+			guildOnly: true,
+		})
+	}
+	run(msg) {
+        //reads user upload from db, then displays
+		try{
+            var toedit = await msg.channel.send('Reading Database.')
+
+            db.get(`${msg.author.id}.uploads`).then(data =>{
+                console.log(data)
+            })
+        } catch (e) {
+            toedit.edit('Error')
+            console.log(e)
+        }
+	}
+};
