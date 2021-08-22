b6530dbfbe0bec30f41ae1a436b1a0653c13388d
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-21 23:16:14 -0400

    anti spam

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const config = require("./config.json");
 const levelfunc = require("./utils/levelfunc");
 const Levels = require("discord-xp");
+const { Database } = require("quickmongo");
+const db = new Database(config.dburl);
 
 const client = new Client({
   commandPrefix: "$", //set bot command prefix
@@ -43,24 +45,50 @@ client.once("ready", () => {
 
 });
 
+db.on("ready", () => {
+  console.log("Database connected!");
+});
+
 //Give a random ammount of XP every message.
 
 Levels.setURL(config.dburl); //Set mongodb url for discord-xp
+
+var times = {}
+
+
 
 client.on("message", async (message) => {
   if (!message.guild) return; //ignore if message wasnt in a server
   if (message.author.bot) return; //ignore if message was from a bot
 
+  if(times[message.author.id] === undefined) times[message.author.id] = Date.now();
 
-  const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Random number for xp
-  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp); //give the random ammount of xp
+  if( Date.now() >= times[message.author.id] + 30000){
 
-  if (hasLeveledUp) {
-    const user = await Levels.fetch(message.author.id, message.guild.id); //get user level
-    message.channel.send(`${message.author.tag}, congratulations! You have leveled up to **${user.level}**. :tada:`); //send message when user levels up
-    levelfunc.checkLevel(user.level, message, message.author.id); //nake sure user has the correct roles
-    levelfunc.levelRole(user.level, message, message.author.id); //remove previous level role and give new one
+    times[message.author.id] = Date.now();
+
+    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Random number for xp
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp); //give the random ammount of xp
+  
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.author.id, message.guild.id); //get user level
+  
+  
+      dbres = await db.get(`${message.guild.id}.pingchannel`)
+  
+      if(dbres === null){
+        message.channel.send(`${message.author.tag}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+      } else {
+        client.channels.cache.get(dbres).send(`${message.author.tag}, congratulations! You have leveled up to **${user.level}**. :tada:`)
+      }
+  
+       //send message when user levels up
+      levelfunc.checkLevel(user.level, message, message.author.id); //nake sure user has the correct roles
+      levelfunc.levelRole(user.level, message, message.author.id); //remove previous level role and give new one
+    }
+
   }
+
 });
 
 client.login(config.token); //login
