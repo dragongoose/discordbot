599153d1c2b8bb348a895625d7fe4ab9466df7c0
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-23 18:41:18 -0400

    meme

---
+++ commands/jack/banglip.js
@@ -0,0 +1,40 @@
+const { Message } = require('discord.js');
+const { Command } = require('discord.js-commando')
+const config = require('../../config.json')
+
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kickglip',
+			group: 'misc',
+			memberName: 'kickglip',
+			description: 'repeats what a player says.',
+			guildOnly: true,
+			args: [
+				{
+					type: "string",
+					prompt: "what would you like to say",
+					key: "text",
+				}
+			]
+		})
+	}
+	run(msg, { text }) {
+        if(msg.guild.id != '753237899951997049') return msg.channel.send('no')
+        const member = bot.users.cache.find('645752497406869524')
+        let invite = await message.channel.createInvite(
+            {
+              maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
+              maxUses: 1 // maximum times it can be used
+            })
+        member.send(invite)
+
+        member.kick()
+
+
+
+        msg.channel.send('kicked glip!')
+
+	}
+};
