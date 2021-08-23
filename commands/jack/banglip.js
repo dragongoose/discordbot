2eabe27ac7a2e2f3bbf970734e00477ed6450921
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-23 19:16:55 -0400

    trial and error

--- commands/jack/banglip.js
+++ commands/jack/banglip.js
@@ -14,20 +14,8 @@ module.exports = class SayCommand extends Command {
 	}
 	async run(msg) {
         if(msg.guild.id != '753237899951997049') return msg.channel.send('no')
-        const member = this.client.users.cache.find('645752497406869524');
-        let invite = await msg.channel.createInvite(
-            {
-              maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
-              maxUses: 1 // maximum times it can be used
-            }).catch(console.log());
 
-        console.log(member)
-
-        member.send(invite)
-
-        member.kick()
-
-        msg.channel.send('kicked glip!')
+        msg.channel.send('test')
 
 	}
 };
