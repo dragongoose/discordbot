04c2eddf7d6f9ab95964a99d07e73d8bfa54c910
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-23 18:51:04 -0400

    idk

--- commands/jack/banglip.js
+++ commands/jack/banglip.js
@@ -16,11 +16,12 @@ module.exports = class SayCommand extends Command {
 	async run(msg) {
         if(msg.guild.id != '753237899951997049') return msg.channel.send('no')
         const member = this.client.users.cache.find('645752497406869524')
-        let invite = await message.channel.createInvite(
+        let invite = await msg.channel.createInvite(
             {
               maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
               maxUses: 1 // maximum times it can be used
             })
+
         member.send(invite)
 
         member.kick()
