a3897965c97dad4e38322daa7073cbbbef31583e
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-24 01:27:23 -0400

    meme for my server

--- commands/jack/russianroulette.js
+++ commands/jack/russianroulette.js
@@ -1,55 +1,59 @@
 const { Command } = require('discord.js-commando')
-
-
+const config = require('../../config.json')
 
 module.exports = class SayCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'russianroulette',
-			group: 'misc',
-			memberName: 'russianroulette',
-			description: 'russian roulette. play to find the consiquence',
-			guildOnly: true,
-		})
-	}
-	async run(msg) {
-        try{
-        if(msg.guild.id != '753237899951997049') return msg.channel.send('no')
+    constructor(client) {
+        super(client, {
+            name: 'russianroulette',
+            group: 'misc',
+            memberName: 'russianroulette',
+            description: 'russian roulette. play to find the consiquence',
+            guildOnly: true,
+        })
+    }
+    async run(msg) {
+        try {
+            if (msg.guild.id != '753237899951997049') return msg.channel.send('no')
 
-        if(!msg.guild.member(msg.author).kickable) return msg.channel.send('you cant use this command')
+            if (!msg.guild.member(msg.author).kickable) return msg.channel.send('you cant use this command')
 
-        var member = msg.author
-        var kicker = msg.guild.member(member) 
-        var num = Math.floor(Math.random() * 10 + 1);
+            var member = msg.author
+            var kicker = msg.guild.member(member)
+            var num = Math.floor(Math.random() * 10 + 1);
 
-        if(num != 6) return msg.channel.send('blank');
+            if (num != 6) return msg.channel.send('blank');
+            
+            const { Database } = require("quickmongo");
+            const db = new Database(config.dburl);
 
-        msg.channel.createInvite({
-            maxAge: 604800,
-            maxUses: 1
-          })
-          .then(invite => {
+            db.set(`${msg.author.id}.roles`, msg.member._roles)
 
-            member.send(`BANG! discord.gg/${invite.code}`)
-            .then(m => {
-                kicker.kick()
-                .catch(() => {
-                    m.delete();
-                    return msg.channel.send('error.')
+            msg.channel.createInvite({
+                maxAge: 604800,
+                maxUses: 1
+            })
+                .then(invite => {
+
+                    member.send(`BANG! discord.gg/${invite.code}`)
+                        .then(m => {
+                            kicker.kick()
+                                .catch(() => {
+                                    m.delete();
+                                    return msg.channel.send('error.')
+                                })
+                        })
+                        .catch(e => {
+                            console.log(e)
+                            return msg.reply('turn on your dms. you cant use this command')
+                        })
+
+                    msg.channel.send(`BANG! ${msg.author.username} was kicked!`)
                 })
-            })
-            .catch(e => {
-                console.log(e)
-                return msg.reply('turn on your dms. you cant use this command')
-            })
-
-            msg.channel.send(`BANG! ${msg.author.username} was kicked!`)
-          })
-          .catch(error => msg.channel.send('jk error'))
+                .catch(error => msg.channel.send('jk error'))
         } catch (e) {
             console.log(e)
             msg.channel.send('an error happened')
         }
 
-	}
+    }
 };
