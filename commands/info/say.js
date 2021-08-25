5ea37d91acf828ba6cf1ce282582df41852f9edf
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-25 19:51:15 -0400

    say command

---
+++ commands/info/say.js
@@ -0,0 +1,31 @@
+const { Message, Client } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    name: "say",
+    aliases: ['repeat'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+
+		if (msg.author.id == config.ownerid) {
+			msg.channel.send(args.join(' '));
+            return msg.delete();
+		} else {
+
+			if (args.join(' ').includes('<@')) return msg.reply('no');
+
+			if (args.join(' ') == '@everyone') {
+				msg.reply('no')
+			} else {
+				msg.channel.send(args.join(' '))
+				msg.delete()
+			}
+		}
+
+    },
+};
