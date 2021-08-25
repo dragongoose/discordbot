26ccf41bdd5111d8b425f98e2962423b67945806
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-25 19:51:21 -0400

    add ping command

---
+++ commands/info/ping.js
@@ -0,0 +1,31 @@
+const { Message, Client, MessageEmbed} = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    //aliases: ['p'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+        // It sends the user "Pinging"
+        msg.channel.send({content: "Pinging..."}).then(m => {
+            // The math thingy to calculate the user's ping
+            var ping = m.createdTimestamp - msg.createdTimestamp;
+
+            // Basic embed
+            var embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('🏓 Latency')
+                .addFields(
+                    { name: 'Bot Latency', value: '```ini\n' + '[' + ping + 'ms' + ']' + '\n```' },
+                    { name: 'API Latency', value: '```ini\n' + '[' + Math.round(client.ws.ping) + 'ms' + ']' + '\n```' },
+                )
+                .setTimestamp()
+            // Then It Edits the message with the ping variable embed that you created
+            m.edit({embeds: [embed]})
+        });
+    },
+};
