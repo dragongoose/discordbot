095b5b083cf80d15231b3ba409ef9896aa2069b0
Dragonshadow14 <dragon@dragongoose.xyz>
2021-08-25 19:51:46 -0400

    add coinflip command

---
+++ commands/info/coinflip.js
@@ -0,0 +1,25 @@
+const { Message, Client } = require("discord.js");
+
+module.exports = {
+    name: "coinflip",
+    aliases: ['random'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+
+        var number = Math.round(Math.random());
+		if (number == '1') {
+			var head = 'Heads!'
+		} else {
+			if (number == '0')
+				var head = 'Tails!'
+		}
+
+		msg.channel.send(head)
+		
+    },
+};
