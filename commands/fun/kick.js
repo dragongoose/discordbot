1dde44e045c78aaaf1b31639994d7eda8dd8da8b
Dragonshadow14 <dragon@dragongoose.xyz>
2021-09-17 20:41:49 -0400

    meme command

---
+++ commands/fun/kick.js
@@ -0,0 +1,36 @@
+const { Client, MessageEmbed } = require("discord.js");
+const DisTube = require("distube");
+const config = require("../../config.json");
+
+module.exports = {
+    name: "fakekick",
+    aliases: ['kick'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+        try{
+            const user = msg.author
+            var member = msg.guild.members.resolve(user);
+            if(!member.kickable()) return msg.channel.send('user not kicable')
+    
+            let invite = await msg.channel.createInvite({
+                maxAge: 604800, //1 day
+                maxUses: 1
+              }).catch(console.error);
+
+            console.log(invite.code)
+    
+            member.send(`lmao baka discord.gg/${invite.code}`).then()
+            member.kick()
+
+        } catch (e) {
+            msg.channel.send('error')
+            console.log(e)
+        }
+
+    },
+};
