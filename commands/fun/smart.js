9adb83fce0a56748aea435a547c02192fbb8d422
Dragonshadow14 <tylerjseverino@gmail.com>
2021-09-21 10:20:40 -0400

    make people smort

---
+++ commands/fun/smart.js
@@ -0,0 +1,27 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: "smart",
+    //aliases: [],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+
+        try {
+            var target = msg.mentions.users.first() || msg.author
+
+            const res = await fetch(`https://api.no-api-key.com/api/v2/smrt?image=${target.avatarURL({ format: 'png' })}`).then(response => response.json());
+
+            msg.reply({ files: [res]})
+        } catch (e) {
+            console.log(e)
+            msg.channel.send('error try again later')
+        }
+
+    },
+};
