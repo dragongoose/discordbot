83d6deb9e86958f8890691c3fcabe0fe0d160057
Dragonshadow14 <dragon@dragongoose.xyz>
2021-10-13 16:38:11 -0400

    Include MongoDB ping

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,4 +1,5 @@
-const { Message, Client, MessageEmbed} = require("discord.js");
+const { Message, Client, MessageEmbed } = require("discord.js");
+const mongoose = require("mongoose");
 
 module.exports = {
     name: "ping",
@@ -12,21 +13,35 @@ module.exports = {
      */
     run: async (client, msg, args) => {
         // It sends the user "Pinging"
-        msg.channel.send({content: "Pinging..."}).then(m => {
+        msg.channel.send({ content: "Pinging..." }).then(m => {
             // The math thingy to calculate the user's ping
             var ping = m.createdTimestamp - msg.createdTimestamp;
 
-            // Basic embed
-            var embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('🏓 Latency')
-                .addFields(
-                    { name: 'Bot Latency', value: '```ini\n' + '[' + ping + 'ms' + ']' + '\n```' },
-                    { name: 'API Latency', value: '```ini\n' + '[' + Math.round(client.ws.ping) + 'ms' + ']' + '\n```' },
-                )
-                .setTimestamp()
-            // Then It Edits the message with the ping variable embed that you created
-            m.edit({embeds: [embed]})
+            var mongoping;
+
+            var begin = Date.now()
+            mongoose.connection.db.admin().ping((err, res) => {
+                if (err) mongoping = '?';
+                var end = Date.now()
+
+                mongoping = end - begin
+
+                // Basic embed
+                var embed = new MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('🏓 Latency')
+                    .addFields(
+                        { name: 'Bot Latency', value: '```ini\n' + '[' + ping + 'ms' + ']' + '\n```', inline: true },
+                        { name: 'API Latency', value: '```ini\n' + '[' + Math.round(client.ws.ping) + 'ms' + ']' + '\n```', inline: true },
+                        { name: 'MongoDB Latency', value: `\`\`\`ini\n [${mongoping}ms] \n\`\`\``, inline: true },
+
+                    )
+                    .setTimestamp()
+                    .setFooter(msg.author.tag, msg.author.avatarURL());
+                // Then It Edits the message with the ping variable embed that you created
+                m.edit({ embeds: [embed] })
+            })
+
         });
     },
 };
