e9c6e40d4e9af98c76de34d8790898d9e9920c2c
Dragonshadow14 <dragon@dragongoose.xyz>
2021-10-23 22:14:39 -0400

    real ban and kick commands

--- commands/fun/ban.js
+++ commands/fun/ban.js
@@ -3,9 +3,9 @@ const DisTube = require("distube");
 const config = require("../../config.json");
 
 module.exports = {
-    name: "kick",
-    description: "kicks a user",
-    //aliases: ['kick'],
+    name: "ban",
+    description: "bans a member",
+    aliases: ['voilate'],
     /**
      *
      * @param {Client} client
@@ -13,7 +13,7 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, msg, args) => {
-        if (!msg.member.permissions.has("KICK_MEMBERS")) return msg.channel.send('No perms!')
+        if (!msg.member.permissions.has("BAN_MEMBERS")) return msg.channel.send('No perms!')
         if (!msg.mentions.users.first()) return msg.channel.send('You must mention who you want to ban!')
         let member = msg.guild.members.cache.get(msg.mentions.users.first().id)
         if (!member) return msg.reply('Please specify a member for me to kick them')
@@ -23,8 +23,8 @@ module.exports = {
 
         
         
-        member.kick(reason)
-        .then(msg.channel.send(`Kicked ${member.user.tag}`))
+        member.ban(reason)
+        .then(msg.channel.send(`banned ${member.user.tag}`))
         .catch(err => console.log(err));
 
 
