307fd8345d885692afe389dd19be68642c799b01
Dragonshadow14 <dragon@dragongoose.xyz>
2021-10-24 20:13:25 -0400

    Whoops, fix errors when guild dint have roles already created

--- commands/managing/setupjoinprotection.js
+++ commands/managing/setupjoinprotection.js
@@ -12,113 +12,129 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, msg, args) => {
+    try {
+      const guildSettings = require("../../schema/guildsettings.js")
+      var guildstorage = await guildSettings.findOne({ guildID: msg.guild.id })
+      const parsed = JSON.parse(JSON.stringify(guildstorage))
 
-    const guildSettings = require("../../schema/guildsettings.js")
-    const guildstorage = await guildSettings.findOne({ guildID: msg.guild.id })
-    const parsed = JSON.parse(JSON.stringify(guildstorage))
+      var edit = await msg.channel.send("Starting....")
 
-    var edit = await msg.channel.send("Starting....")
+      if (!msg.member.permissions.has('MANAGE_GUILD')) return msg.reply('No Perms!');
+      if (parsed && parsed.settingsJson.joinprotection) return msg.channel.send('Already setup!')
 
-    if (!msg.member.permissions.has('MANAGE_GUILD')) return msg.reply('No Perms!');
-    if (parsed.settingsJson.joinprotection) return msg.channel.send('Already setup!')
+      const embed = new MessageEmbed()
+        .setColor('#808080')
+        .setTitle(`Setup`)
+        .setDescription('I will need to do the following to setup join protection: \n Make 3 roles, inviter, interviewer, new kid, and member. This must not be touched after being created \n I will create a new channel everytime a member joins. It will be deleted after the user is accepted/declined \n YOU must make every channel so @everyone cant view, but the member role can. Reply with the channel id to log joins into if this is ok.')
+        .setTimestamp()
+        .setFooter(msg.author.tag, msg.author.avatarURL());
 
-    const embed = new MessageEmbed()
-      .setColor('#808080')
-      .setTitle(`Setup`)
-      .setDescription('I will need to do the following to setup join protection: \n Make 3 roles, inviter, interviewer, new kid, and member. This must not be touched after being created \n I will create a new channel everytime a member joins. It will be deleted after the user is accepted/declined \n YOU must make every channel so @everyone cant view, but the member role can. Reply with the channel id to log joins into if this is ok.')
-      .setTimestamp()
-      .setFooter(msg.author.tag, msg.author.avatarURL());
+      edit.edit({ embeds: [embed] })
 
-    edit.edit({ embeds: [embed] })
+      let filter = m => m.author.id === msg.author.id
 
-    let filter = m => m.author.id === msg.author.id
+      //Created all needed things
+      var newkidrole = msg.guild.roles.cache.find(role => role.name === "new kid");
+      var inviterrole = msg.guild.roles.cache.find(role => role.name === "inviter");
+      var interviewerrole = msg.guild.roles.cache.find(role => role.name === "interviewer");
+      var memberrole = msg.guild.roles.cache.find(role => role.name === "member");
 
-    //Created all needed things
-    var newkidrole = msg.guild.roles.cache.find(role => role.name === "new kid");
-    var inviterrole = msg.guild.roles.cache.find(role => role.name === "inviter");
-    var interviewerrole = msg.guild.roles.cache.find(role => role.name === "interviewer");
-
-    var logchannel;
+      var logchannel;
 
 
 
-    const makeroles = new Promise((async (resolve, reject) => {
-      if (!newkidrole) {
-        newkidrole = await message.guild.roles.create({
-          data: {
+      const makeroles = new Promise((async (resolve, reject) => {
+        if (!newkidrole) {
+          newkidrole = await msg.guild.roles.create({
             name: 'new kid',
             reason: 'the role is needed for join protection'
-          }
-        }).catch(err => reject(err))
-      }
+          }).catch(err => reject(err))
+        }
 
-      if (!inviterrole) {
-        inviterrole = await message.guild.roles.create({
-          data: {
+        if (!inviterrole) {
+          inviterrole = await msg.guild.roles.create({
             name: 'inviter',
             reason: 'the role is needed for join protection'
-          }
-        })
-      }
+          })
+        }
 
-      if (!interviewerrole) {
-        interviewerrole = await message.guild.roles.create({
-          data: {
+        if (!interviewerrole) {
+          interviewerrole = await msg.guild.roles.create({
             name: 'interviewer',
             reason: 'the role is needed for join protection'
-          }
-        }).catch(err => reject(err))
-      }
-      resolve()
-    }))
+          }).catch(err => reject(err))
+        }
 
-    const collector = msg.channel.createMessageCollector({ filter, time: 15000 });
+        if (!memberrole) {
+          memberrole = await msg.guild.roles.create({ name: 'member', reason: 'the role is needed for join protection' }).catch(err => reject(err))
+        }
+        resolve()
+      }))
 
-    collector.on('collect', message => {
-      console.log(`Collected ${message.content}`);
-      console.log(message.content)
-      var channel = client.guilds.cache.get(msg.guild.id).channels.cache.get(message.content)
-      if (!channel) return msg.channel.send('I couldnt find the channel');
+      const collector = msg.channel.createMessageCollector({ filter, time: 15000 });
 
-      logchannel = message.content
+      collector.on('collect', message => {
+        console.log(`Collected ${message.content}`);
+        console.log(message.content)
+        var channel = client.guilds.cache.get(msg.guild.id).channels.cache.get(message.content)
+        if (!channel) return msg.channel.send('I couldnt find the channel');
 
-      console.log(logchannel)
+        logchannel = message.content
 
-      guildSettings.findOne({ guildID: msg.guild.id })
-        .then(set => {
-          var parsed = JSON.parse(JSON.stringify(set))
+        console.log(logchannel)
 
-          var oldsettings = parsed.settingsJson
-          var newsettings = oldsettings
+        guildSettings.findOne({ guildID: msg.guild.id })
+          .then(set => {
+            var parsed = JSON.parse(JSON.stringify(set))
 
-          console.log(newsettings)
-
-          newsettings.joinprotection = {
-            newkid: newkidrole,
-            inviterrole: inviterrole,
-            interviewer: interviewerrole,
-            logchannel: logchannel,
-          }
-
-          set.guildID = msg.guild.id;
-          set.settingsJson = newsettings
-
-          set.markModified('guildID');
-          set.markModified('settingsJson');
-
-          set.save();
-          return msg.channel.send('done!')
-
-        });
-    });
+            if (!parsed) {
+              var emptysettings = new guildSettings({
+                guildID: msg.guild.id,
+                settingsJson: {
+                  joinprotection: {
+                    newkid: newkidrole,
+                    member: memberrole,
+                    inviterrole: inviterrole,
+                    interviewer: interviewerrole,
+                    logchannel: logchannel,
+                  }
+                },
+              })
+              emptysettings.save();
+              return msg.channel.send('Set')
+            } else {
+              var oldsettings = parsed.settingsJson;
+              var newsettings = oldsettings
 
 
-    collector.on('end', collected => {
-      msg.channel.send(`Out of time. Try again.`);
-    });
+              newsettings.joinprotection = {
+                newkid: newkidrole,
+                member: memberrole,
+                inviterrole: inviterrole,
+                interviewer: interviewerrole,
+                logchannel: logchannel,
+              }
+
+              set.guildID = msg.guild.id;
+              set.settingsJson = newsettings
+
+              set.markModified('guildID');
+              set.markModified('settingsJson');
+
+              set.save();
+              return msg.channel.send('done!')
+            }
+          });
+      });
 
 
-
+      collector.on('end', collected => {
+        msg.channel.send(`Out of time. Try again.`);
+      });
+    } catch (e) {
+      console.log(e)
+      msg.channel.send("There was a error")
+    }
 
   },
 };
