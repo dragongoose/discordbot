da31858bb8c5d9f82a4ebe1db0bd0654ae046136
Dragonshadow14 <dragon@dragongoose.xyz>
2021-11-03 21:15:46 -0400

    Command where the bot can reply to a user.

---
+++ commands/fun/reply.js
@@ -0,0 +1,22 @@
+const { Message, Client } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    name: "reply",
+    description: "replies to a message",
+    //aliases: ['repeat'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, msg, args) => {
+
+        if(!msg.reference) return msg.channel.send('You must reply to a message!')
+        const message = await msg.channel.messages.fetch(msg.reference.messageId)
+        if(!message) return msg.channel.send("I couldn't find the messsage")
+
+        message.reply(args.join(" "))
+    },
+};
