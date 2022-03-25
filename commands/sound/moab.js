0a8049919122e2960992b1d0dbab45addaeb79f3
dragongoose <tylerjseverino@gmail.com>
2022-03-25 16:12:16 -0400

    Minor fix

--- commands/sound/moab.js
+++ commands/sound/moab.js
@@ -34,7 +34,7 @@ module.exports = {
                         newChannel.send('https://tenor.com/view/explosion-mushroom-cloud-atomic-bomb-bomb-boom-gif-4464831')
                         msg.channel.overwritePermissions([
                             {
-                                id: guild.id,
+                                id: interaction.guild.id,
                                 deny: [Permissions.FLAGS.VIEW_CHANNEL],
                             },
                             {
@@ -42,6 +42,8 @@ module.exports = {
                                 allow: [Permissions.FLAGS.VIEW_CHANNEL],
                             },
                         ])
+                        message.delete()
+                        msg.delete()
                         break;
                     case '❌':
                         message.delete()
