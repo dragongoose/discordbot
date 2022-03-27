78492e175f70ed5bc20adbaa3f97dfbe7409cddb
dragongoose <dragon@dragongoose.xyz>
2022-03-27 12:26:12 -0400

    queue now has multiple pages

--- commands/sound/queue.js
+++ commands/sound/queue.js
@@ -1,5 +1,6 @@
 const { Client, MessageEmbed } = require("discord.js");
 const DisTube = require("distube");
+const paginator = require("../../utils/pageinator.js");
 
 module.exports = {
     name: "queue",
@@ -15,11 +16,11 @@ module.exports = {
 
         // Check if user is in vc
         if (!msg.member.voice.channel) return msg.channel.send('You must be in a voice channel.')
-
+        
         let queue = client.distube.getQueue(msg);
         if (queue === undefined) return msg.channel.send('The queue is empty!')
 
-        var text;
+        var text = [];
 
         function truncate(str, n) {
             return (str.length > n) ? str.substr(0, n - 1) + '..' : str;
@@ -27,23 +28,29 @@ module.exports = {
 
         for (let i = 0; i < queue.songs.length; i++) {
             var songname = truncate(queue.songs[i].name, 35)
-            if (text == undefined) {
-                text = i + 1 + ') ' + songname + ' - `' + queue.songs[i].formattedDuration + '`' + '\n'
-            } else {
-                text += i + 1 + ') ' + songname + ' - `' + queue.songs[i].formattedDuration + '`' + '\n'
-            }
-
-
+            text.push(`${i + 1}. ${songname} - ${queue.songs[i].formattedDuration}`) 
         }
 
+        // Split array into chunks of 10
+        const chunkSize = 10;
+        var chunks = [];
+        for (var i = 0; i < text.length; i += chunkSize) {
+            chunks.push(text.slice(i, i + chunkSize));
+        }
 
-        const embed = new MessageEmbed()
-            .setTitle(`**Current Queue**`)
-            .setDescription(text)
-            .setColor(0xD53C55) // Green: 0x00AE86
-            .setFooter('hi :3')
-            .setTimestamp();
+        let pages = [];
 
-        msg.channel.send({ embeds: [embed] })
+        // Loop through chunks
+        for (let i = 0; i < chunks.length; i++) {
+            let embed = new MessageEmbed()
+            .setTitle('**Queue:**')
+                .setDescription(`\`\`\`sql\n${chunks[i].join("\n")}\`\`\``)
+                .setColor(0xD53C55) // Green: 0x00AE86
+                .setFooter({ text: 'hi :3' })
+                .setTimestamp();
+            pages.push(embed)
+        }
+
+        paginator.main(pages, msg, 0)
     }
 };
