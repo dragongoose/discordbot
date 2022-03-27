81ff14f71a147e822a6f506834e4feb327564d0e
dragongoose <dragon@dragongoose.xyz>
2022-03-27 12:25:55 -0400

    Working remove command

--- commands/sound/remove.js
+++ commands/sound/remove.js
@@ -22,7 +22,23 @@ module.exports = {
         // Check if arg is within queue size
         if (args[0] - 1 > queue.songs.length) return msg.channel.send('Please enter a valid number.')
 
-        console.log(queue.songs[args[0] - 1])
+        if (args[0] - 1 === 0) {
+            return msg.channel.send("Cannot remove a song that's playing!")
+        }
+
+        let removed = queue.songs[args[0] - 1]
+        console.log(queue.songs.splice(args[0] - 1, 1))
+        const embed = new MessageEmbed()
+            .setDescription(`**Removed: ${removed.name}**`)
+            .addFields(
+                { name: 'Requested by:', value: `<@${removed.user.id}>`, inline: true },
+                { name: 'Duration', value: removed.formattedDuration, inline: true },
+                { name: 'Removed by:', value: `<@${msg.author.id}>`, inline: true }
+            )
+            .setColor(0xD53C55) // Green: 0x00AE86
+            .setFooter({ text: 'hi :3' })
+            .setTimestamp();
+        return msg.channel.send({ embeds: [embed]})
 
 
     },
