91990ce06e3e415e2482dc5c17ba8213214826e7
dragongoose <dragon@dragongoose.xyz>
2022-04-21 21:29:51 -0400

    trolling

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,6 +1,5 @@
 const { Message, Client, MessageEmbed } = require("discord.js");
 const config = require("../../config.json");
-const userEconomy = require("../../schema/userEconomy.js")
 
 module.exports = {
     name: "balance",
@@ -13,25 +12,6 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, msg, args) => {
-
-        let target = args[0] || msg.author.id || msg.mentions.users.first().id 
-
-        const userbal = await userEconomy.findOne({ userID: target });
-
-        if (userbal === null) {
-            var newbal = new userEconomy({
-                userID: target,
-                balance: 0,
-                items: [],
-                userstorage: []
-            })
-            await newbal.save();
-            msg.channel.send('Added you to the databse, run the command agian.')
-        }
-
-        var parsed = JSON.parse(JSON.stringify(userbal))
-
-        msg.reply(`Your balance is $${parsed.balance}`)
-
+        msg.reply(`Your balance is $${Math.random() * 1000000}`)
     },
 };
